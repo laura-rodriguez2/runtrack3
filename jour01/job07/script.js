d29e2e446d71a4259f8d3d35990cc40d0fbd3863
Laura <laura.rodriguez@laplateforme.io>
2022-03-14 16:48:51 +0100

    job07 fini

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,15 +1,30 @@
-function fizzbuzz(){
-        for (let i = 1; i < 152; i++) {
-            if((i%3 == 0) && (i%5 == 0)){
-                console.log("FizzBuzz");
-            } else if (i%3 == 0){
-                console.log("Fizz");
-            }  else if(i%5 == 0){
-                console.log("Buzz");
-            } else {
-                console.log(i);
-            }
-        }
+function jourtravaille(date) {
+    var date = new Date(date)
+    var date_fr = date.toLocaleDateString('fr-FR'); //date en français
+    var ferie = [
+        new Date(2020, 01, 01),
+        new Date(2020, 04, 13),
+        new Date(2020, 05, 01),
+        new Date(2020, 05, 08),
+        new Date(2020, 05, 21),
+        new Date(2020, 06, 01),
+        new Date(2020, 07, 14),
+        new Date(2020, 08, 15),
+        new Date(2020, 11, 01),
+        new Date(2020, 11, 11),
+        new Date(2020, 12, 25),
+    ];
+    //Ferie
+    if (ferie == true) {
+        console.log("Le " + date_fr + " est un jour férié");
     }
-    
-fizzbuzz();
+    //Weekend
+    else if (date.getDay() == 6 || date.getDay() == 0) {
+        console.log("Non, Le " + date_fr + " est un weekend");
+    }
+    //Jour de travail
+    else {
+        console.log("Oui, Le " + date_fr + " est un jour de travail");
+    }
+}
+jourtravaille("2022-03-13")
