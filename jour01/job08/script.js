0ce3d0500c940131dfe6843296475b6e83010f57
Laura <laura.rodriguez@laplateforme.io>
2022-03-14 17:40:42 +0100

    job08 fini

---
+++ jour01/job08/script.js
@@ -0,0 +1,21 @@
+function sommenombrespremiers(nb1, nb2) {
+
+    var i = 0;
+
+    //nb1
+    for (i; i < nb1; i++) {
+        if (nb1 % i == 0) {
+            return false;
+        }
+    }
+    //nb2
+    for (i; i < nb2; i++) {
+        if (nb2 % i == 0) {
+            return false;
+        }
+    }
+    //resultat
+    var resultat = nb1 + nb2;
+    console.log(resultat);
+}
+console.log(sommenombrespremiers(1, 3))
