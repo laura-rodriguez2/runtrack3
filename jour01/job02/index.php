4fc8b0d7c8fca242aefb0637b9822a0d4f185e17
Laura <laura.rodriguez@laplateforme.io>
2022-03-14 11:28:02 +0100

    job02 fini

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JavaScript</title>
-    <script>
-        alert('Hello JavaScript!')
+    <script language="javascript" type="text/javascript" src="script.js">
     </script>
 </head>
+
 <body>
-    
+    <script>
+        popup()
+    </script>
+
 </body>
+
 </html>
