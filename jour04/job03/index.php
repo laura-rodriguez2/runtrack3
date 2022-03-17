b683762b5d9265d3efc8c2018cb5da7e9cc66aba
Laura <laura.rodriguez@laplateforme.io>
2022-03-17 19:14:45 +0100

    j4 job03 maj

---
+++ jour04/job03/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript</title>
+    <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+    <script type="text/javascript" src='script.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+
+    </script>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <form>
+            <label>id</label>
+            <input type="text">
+            <label>nom</label>
+            <input type="text">
+            <label>type</label>
+            <input type="button" id="filtrer" value="filtrer">
+        </form>
+        <script type="text/javascript" src="script.js"></script>
+    </main>
+    <footer>
+
+
+    </footer>
+</body>
+
+</html>
