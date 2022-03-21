366318ceebbc8647fb6bca010f2c121ddfc37663
Laura <laura.rodriguez@laplateforme.io>
2022-03-21 15:25:59 +0100

    j6 job04 fini

---
+++ jour06/job04/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Media Queries</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" for="menu__toggle">
+                <span></span>
+            </label>
+            
+            <ul class="menu__box">
+                <li><a class="menu__item" href="">Accueil</a></li>
+                <li><a class="menu__item"href="">Présentation</a></li>
+                <li><a class="menu__item"href="">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+</body>
+
+</html>
