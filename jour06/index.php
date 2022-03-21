8106dde59cbc381f9c6386c4108092b6bc296af3
Laura <laura.rodriguez@laplateforme.io>
2022-03-21 11:23:15 +0100

    j6 job01 fini

--- jour06/index.php
+++ jour06/index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Media Queries</title>
+
+    <link rel="stylesheet" type="text/css" media="screen" href="style1.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width:1680px) and (max-width:1920px)" href="style2.css">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width:1280px) and (max-width:1680px)" href="style3.css">
+    <link rel="stylesheet" type="text/css" media="screen and (max-width:1280px)" href="style4.css">
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
